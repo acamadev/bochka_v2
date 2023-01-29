06967601db13bcbdb2b7e23cbd2c29b96361e90f
acamadev <74523975+acamadev@users.noreply.github.com>
2023-01-29 13:26:38 +0500

    Create bash.js

---
+++ SlashCommands/Test/bash.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const shell = require("child_process")
+
+module.exports = {
+    name: 'bash',
+    description: 'shell',
+    Globally: true,
+    Permissions: [Discord.PermissionFlagsBits.Administrator],
+    options: [
+        {
+            name: 'args',
+            description: '1',
+            type: Discord.ApplicationCommandOptionType.String,
+            required: true
+        }],
+
+        run: async (ankai, inter) => {
+            try {
+                const e = await shell.execSync(`${inter.options.getString('message')}`).toString()
+                inter.reply({ content:`${e}`, ephemeral: true })
+            } catch (err) {
+                inter.reply({ content: `An error occurred: ${err}`, ephemeral: true })
+            }
+        }
+    }
